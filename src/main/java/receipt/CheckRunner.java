bfc373cda641475da2a0d7f433ee79dea0aea6db
jekuz <jekuz@mail.ru>
2022-05-24 16:14:52 +0300

    DataMap generic interface implementation

--- src/main/java/receipt/CheckRunner.java
+++ src/main/java/receipt/CheckRunner.java
@@ -14,14 +14,14 @@ public class CheckRunner {
         Data data = Args.parser(args);
 
         // получаем объекты данных
-        DataMap<Integer, Product> pList = new ProductList(data.productsFileName);
-        DataMap<String, Integer> cList = new CardList(data.cardsFileName);
+        DataMap<Integer, Product> productList = new ProductList(data.productsFileName);
+        DataMap<String, Integer> cardList = new CardList(data.cardsFileName);
 
         // проверка указаний аргументов в объектах данных
-        Args.check(data.products, data.cardNumber, pList, cList);
+        Args.check(data.products, data.cardNumber, productList, cardList);
 
         // формируем чек
-        Form form = new FormBuilder(Calc.result(data, pList, cList));
+        Form form = new FormBuilder(Calc.result(data, productList, cardList));
 
         form.print();
         form.save(String.format("receipt-%04d.txt", Result.receiptNumber ));
